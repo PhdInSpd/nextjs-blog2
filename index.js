1823316bc9d209232839450113aa5310b6cf0189
Chaver Yadim <51681259+PhdInSpd@users.noreply.github.com>
2022-09-15 04:05:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// open create-next-app
+// nextjs-blog is the name of MY application
+// npx create-next-app nextjs-blog --use-npm --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
+
+// installed
+// nodes_modules is framwork used by next.js
